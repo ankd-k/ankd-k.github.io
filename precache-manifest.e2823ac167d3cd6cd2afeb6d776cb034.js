2fd789e3683708b98c3a7df7b67177efe283a198
ankd-k <ankd.2653@gmail.com>
2019-09-23 15:45:12 +0900

    first commit

---
+++ precache-manifest.e2823ac167d3cd6cd2afeb6d776cb034.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a643f2f9ffbe2387e2d00aad91505d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c372e33c29089d616a",
+    "url": "/static/css/main.2bc20a9b.chunk.css"
+  },
+  {
+    "revision": "327ad870a11bbb6fa4f7",
+    "url": "/static/js/2.efd8d765.chunk.js"
+  },
+  {
+    "revision": "03c372e33c29089d616a",
+    "url": "/static/js/main.352a2465.chunk.js"
+  },
+  {
+    "revision": "416cc1c79b8c22940a8c",
+    "url": "/static/js/runtime~main.7302273d.js"
+  },
+  {
+    "revision": "f5e1f5a1e695f9481983e5c196b251a2",
+    "url": "/static/media/URWDIN-Black.f5e1f5a1.otf"
+  },
+  {
+    "revision": "e2fa61ece94657e0ddfcc73d5fcd42c5",
+    "url": "/static/media/URWDIN-Light.e2fa61ec.otf"
+  }
+]);
